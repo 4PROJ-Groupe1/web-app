133eec96bc10a8b9eac24126c577b879f827ccda
Paul Riviere <paul.riviere@supinfo.com>
2021-06-08 16:25:30 +0200

    register message erreur
    ajout snackbar quand erreur de register (doublon mail)

--- webapp/src/app/views/sessions/SignUp.jsx
+++ webapp/src/app/views/sessions/SignUp.jsx
@@ -9,6 +9,94 @@ import {
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { connect } from "react-redux";
 import UserService from "../../services/UserService";
+import Snackbar from "@material-ui/core/Snackbar";
+import SnackbarContent from "@material-ui/core/SnackbarContent";
+import clsx from "clsx";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import WarningIcon from "@material-ui/icons/Warning";
+import ErrorIcon from "@material-ui/icons/Error";
+import InfoIcon from "@material-ui/icons/Info";
+import {makeStyles} from "@material-ui/core/styles";
+import {amber, green} from "@material-ui/core/colors";
+import {PropTypes} from "prop-types";
+
+const variantIcon = {
+  success: CheckCircleIcon,
+  warning: WarningIcon,
+  error: ErrorIcon,
+  info: InfoIcon
+};
+
+const useStyles1 = makeStyles(theme => ({
+  success: {
+    backgroundColor: green[600]
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark
+  },
+  info: {
+    backgroundColor: theme.palette.primary.main
+  },
+  warning: {
+    backgroundColor: amber[700]
+  },
+  icon: {
+    fontSize: 20
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1)
+  },
+  message: {
+    display: "flex",
+    alignItems: "center"
+  }
+}));
+
+function MySnackbarContentWrapper(props) {
+  const classes = useStyles1();
+  const { className, message, onClose, variant, ...other } = props;
+  const Icon = variantIcon[variant];
+
+  return (
+      <SnackbarContent
+          className={clsx(classes[variant], className)}
+          aria-describedby="client-snackbar"
+          message={
+            <span id="client-snackbar" className={classes.message}>
+          <Icon className={clsx(classes.icon, classes.iconVariant)} />
+              {message}
+        </span>
+          }
+          action={[
+            <IconButton
+                key="close"
+                aria-label="Close"
+                color="inherit"
+                onClick={onClose}
+            >
+              <CloseIcon className={classes.icon} />
+            </IconButton>
+          ]}
+          {...other}
+      />
+  );
+}
+
+MySnackbarContentWrapper.propTypes = {
+  className: PropTypes.string,
+  message: PropTypes.node,
+  onClose: PropTypes.func,
+  variant: PropTypes.oneOf(["success", "warning", "error", "info"]).isRequired
+};
+
+const useStyles2 = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1)
+  }
+}));
 
 class SignUp extends Component {
   state = {
@@ -16,7 +104,9 @@ class SignUp extends Component {
     surname: "",
     email: "",
     password: "",
-    agreement: ""
+    agreement: "",
+    displayError: false,
+    errorMessage: ""
   };
 
   handleChange = event => {
@@ -35,7 +125,10 @@ class SignUp extends Component {
               console.log('register response : ', response);
               this.props.history.push("/session/signin");
             } else {
-              console.log("register failed : ", response);
+              console.log("register failed : ", response.error);
+              //TODO: snackbar avec message
+              this.setState({displayError: true});
+              this.setState({errorMessage: response.error});
             }
           },
           error => {
@@ -48,10 +141,34 @@ class SignUp extends Component {
       }
     );
   };
+
+  handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    this.setState({displayError: false});
+  }
+
   render() {
-    let { name, surname, email, role, password } = this.state;
+    let { name, surname, email, password } = this.state;
+    let { classes } = this.props;
     return (
       <div className="signup flex flex-center w-100 h-100vh">
+        <Snackbar
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left"
+            }}
+            open={this.state.displayError}
+            autoHideDuration={6000}
+            onClose={this.handleClose}
+        >
+          <MySnackbarContentWrapper
+              variant="error"
+              //className={classes.margin}
+              message={this.state.errorMessage}
+          />
+        </Snackbar>
         <div className="p-8">
           <Card className="signup-card position-relative y-center">
             <Grid container>
